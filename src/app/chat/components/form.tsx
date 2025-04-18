46015dcf43cfc59883f0b05e5f335c8a25abd915
asmamaw <akassahun2023@gmail.com>
2025-04-18 22:36:44 +0300

    completing chat form

--- src/app/chat/components/form.tsx
+++ src/app/chat/components/form.tsx
@@ -1,5 +1,9 @@
 import { useSession } from "next-auth/react";
 import React, { useRef, useState } from "react";
+import { FileUploader } from "@/components/file-uploader";
+import { postData } from "./action";
+import { Paperclip } from "lucide-react";
+import { Button } from "@/components/ui/button";
 
 
 interface Props {
@@ -21,12 +25,80 @@ const ChatForm:React.FC<Props> = ({recipient_id}) => {
   //@ts-ignore
   const userType = session?.user?.image?.type;
 
+  const HandleFileSend = (url: string, fileType: string) => {
+    if (fileType === "image/png") {
+      console.log("file type");
+      fileType = "png";
+    }
+    if (fileType == "application/pdf") {
+      fileType = "pdf";
+    }
+
+    const fileData = {
+      recipient_id: recipient_id,
+      message: url,
+      fileType: fileType,
+      messageType: "file",
+    };
+    //@ts-ignore
+    postData(undefined, fileData);
+  };
+
+  const [isModalOpen, setIsModalOpen] = useState(false);
+
+  const handleOpenModal = () => {
+    setIsModalOpen(true);
+  };
+
+  const handleCloseModal = () => {
+    setIsModalOpen(false);
+  };
 
   return (
     <form action="">
-      <div>
-        <div>
-          
+      <div className="flex flex-row items-center ">
+        <div className=" cursor-pointer">
+          {
+            open ? (
+              <FileUploader
+              
+              onUploadComplete={(urls) => {
+                console.log(res[0].url, res[0].type);
+                HandleFileSend(res[0].url, res[0].type);
+                setOpen(!open);
+              }}
+              maxFiles={5}
+              maxSize={4}
+              fileTypes={["image", "pdf"]}
+              />
+            ) : (
+              <Paperclip onClick={() => setOpen(!open)} />
+            )
+          }
+        </div>
+        <input
+          type="text"
+          name="message"
+          placeholder="Type your message..."
+          className="flex-grow py-2 px-4 outline-none"
+        />
+        <div className="flex items-center ">
+          <button
+            type="submit"
+            className="bg-teal-500 hover:bg-teal-600 text-white py-2 px-4 rounded-full mr-2"
+          >
+            Send
+          </button>
+          {userType === "lawyer" ? (
+            <Button
+              onClick={handleOpenModal}
+              className="bg-teal-500 hover:bg-teal-600 text-white py-2 px-4 rounded-full"
+            >
+              Create Offer
+            </Button>
+          ) : (
+            ""
+          )}
         </div>
       </div>
     </form>
