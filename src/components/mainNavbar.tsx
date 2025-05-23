69b6812a62b334d9814a671cbf62108d4fa6fecf
asmamaw <akassahun2023@gmail.com>
2025-05-24 02:33:33 +0300

    enhancing client page

--- src/components/mainNavbar.tsx
+++ src/components/mainNavbar.tsx
@@ -489,7 +489,7 @@ const MainNavbar = () => {
         <h1 className="w-full text-3xl font-bold text-white m-4">EQUILEX</h1>
 
         <li className="p-4 border-b rounded-xl hover:bg-[#00df9a] duration-300 hover:text-black cursor-pointer border-gray-600">
-          <Link href={userType === "client" ?"/app/client/notification": "/app/lawyer/notification"}>Notification</Link>
+          <Link href={userType === "client" ?"/client/notification": "/lawyer/notification"}>Notification</Link>
         </li>
         <li className="p-4 border-b rounded-xl hover:bg-[#00df9a] duration-300 hover:text-black cursor-pointer border-gray-600">
           <Link href={"/chat"}>Chat</Link>
@@ -500,7 +500,7 @@ const MainNavbar = () => {
         </li>{
           userType == "client" ? (
             <li className="p-4 border-b rounded-xl hover:bg-[#00df9a] duration-300 hover:text-black cursor-pointer border-gray-600">
-          <Link href={"/app/client/case"}>Cases</Link>
+          <Link href={"/client/case"}>Cases</Link>
         </li>
           ):
           ""
